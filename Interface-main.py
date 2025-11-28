7dafd21fce71ab0ac929958733bbd4680b4a80bc
Embrex6 <166494065+Embrex6@users.noreply.github.com>
2025-11-28 17:46:41 +0100

    ajout d'une fonction pour l'etat de l'enfant

--- Interface-main.py
+++ Interface-main.py
@@ -95,9 +95,99 @@ def mode_bu():
 
         sleep(50)
 
+def mode_baby():
+    display.scroll("BABY")
+
+    while True:
+        if retour_menu():
+            return
+
+        # a = activer babyphone enfant
+        if button_a.was_pressed():
+            radio.send("ON")
+            display.scroll("ON")
+
+            # attendre confirmation
+            start = running_time()
+            confirmed = False
+
+            while running_time() - start < 2000:
+                msg = radio.receive()
+                if msg == "ACK":
+                    confirmed = True
+                    break
+
+            if confirmed:
+                display.scroll("OK")
+            else:
+                display.scroll("NO")
+
+        # B = désactiver babyphone enfant
+        if button_b.was_pressed():
+            radio.send("OFF")
+            display.scroll("OFF")
+
+        sleep(100)
+
+
+
+def mode_etat():
+    display.scroll("ETAT")
+
+    while True:
+
+        if pin_logo.is_touched(): #retour menu
+            start = running_time()
+            while pin_logo.is_touched():
+                if running_time() - start >= 5000:
+                    display.show("M")
+                    sleep(500)
+                    return
+
+        # a = activer la surveillance
+        if button_a.was_pressed():
+            radio.send("ETAT_ON")
+            display.scroll("ON")
+
+            
+            start = running_time() # attendre qu'on recoit la confirmation de l'autre babyphone comme quoi il est connecté
+            confirme = False
+
+            while running_time() - start < 2000:
+                reponse = radio.receive()
+                if reponse == "OK":
+                    confirme = True
+                    break
+
+            if confirme:
+                display.scroll("OK")
+            else:
+                display.scroll("NON")
+
+        # b = desactive la surveillance comme proctection il faut rester appuyé 5 secondes
+        if button_b.is_pressed():
+            start = running_time()
+            while button_b.is_pressed():
+                if running_time() - start >= 5000:
+                    radio.send("ETAT_OFF")
+                    display.scroll("OFF")
+                    sleep(500)
+                    break
+
+        # réception état du bébé
+        msg = radio.receive()
+        if msg in ["D", "B", "A"]: # [détendu ; bouge ;agitation forte ]
+            display.show(msg)
+            if msg == "A":     # agitation forte
+                music.play(['C5:4'], wait=False)
+
+        sleep(100)
+
+
+
 #Liste des modes
 
-modes = ['MILK', 'PERDU', 'PERE' , 'BU']
+modes = ['MILK', 'PERDU', 'PERE' , 'BU' , 'ETAT']
 index = 0
 premier_affichage = True    
 
@@ -147,6 +237,11 @@ while True:
         elif modes[index] == 'BU':
             display.scroll('BU')
             mode_bu()
+
+        elif modes[index] == 'ETAT':
+            display.scroll('ETAT')
+            mode_etat()
+
 
         premier_affichage = True
     
